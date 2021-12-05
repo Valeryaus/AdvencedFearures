6203df684dc4284c904d1f06d2c5ae87cc9fbf5b
Valery <valeryaus.game@gmail.com>
2021-12-05 15:29:53 +0200

    Tests ready for except - "ArithmeticException exception = assertThrows" is not working yet.

--- src/main/java/testingRecap/Calculator.java
+++ src/main/java/testingRecap/Calculator.java
@@ -2,20 +2,47 @@ package testingRecap;
 
 import org.apache.log4j.Logger;
 
+import java.util.Arrays;
+
 public class Calculator {
 
     private static final Logger LOGGER = Logger.getLogger(Calculator.class);
 
-    public int add(int firstNumber, int secondNumber){
+    public int add(int firstNumber, int secondNumber) {
         LOGGER.info("Adding two numbers " + firstNumber + " and " + secondNumber);
         return firstNumber + secondNumber;
     }
 
-    public int addMultipleNumbers(int[] numbers){
+    public int addMultipleNumbers(int[] numbers) {
+        LOGGER.info("Adding numbers" + Arrays.toString(numbers));
         int totalSum = 0;
-        for (int i = 0; i < numbers.length; i++) {
-            totalSum+= numbers[i];
+        for (int number : numbers) {
+            totalSum += number;
         }
         return totalSum;
+    }
+
+    public int subtractTwoNumbers(int thirdNumber, int forthNumber) {
+        LOGGER.info("Subtracting two numbers " + thirdNumber + " and " + forthNumber);
+        return thirdNumber - forthNumber;
+    }
+
+    public int subtractingMultipleNumbers(int[] numbers) {
+        LOGGER.info("Subtracting" + Arrays.toString(numbers));
+        int totalSum = numbers[0];
+
+        for (int i = 1; i < numbers.length; i++) {
+            totalSum -= numbers[i];
+        }
+        return totalSum;
+
+    }
+
+    public int multiply(int firstNumber, int secondNumber) {
+        return firstNumber * secondNumber;
+    }
+
+    public double divide(double thirdNumber, double fourthNumber) {
+        return thirdNumber / fourthNumber;
     }
 }
--- src/test/java/testingRecap/CalculatorTest.java
+++ src/test/java/testingRecap/CalculatorTest.java
@@ -2,13 +2,108 @@ package testingRecap;
 
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 class CalculatorTest {
 
+    private Calculator calculator = new Calculator();
+
     @Test
-    void add() {
+    void testAddTwoNumbers() {
+        //given
+        int firstNumber = 12;
+        int secondNumber = 3;
+
+        //when
+        int actualAddResult = calculator.add(firstNumber, secondNumber);
+
+        //then
+        assertEquals(15, actualAddResult); //JUNIT assertEquals
+        assertThat(actualAddResult).isEqualTo(15); //Tas pats su ASSERTJ biblioteka
+
     }
 
     @Test
-    void addMultipleNumbers() {
+    void testAddMultipleNumbers() {
+        //given
+        int[] numbertsToTest = {5, 2, 6, 1};
+
+        //when
+        int actualResults = calculator.addMultipleNumbers(numbertsToTest);
+
+        //then
+        assertEquals(14, actualResults); // pvz dviem variantais
+        assertThat(actualResults).isEqualTo(14); // pvz dviem variantais, tai naudojam ta kuris labiau patinka
     }
-}
+
+    @Test
+    void testSubtractTwoNumbers() {
+        //given
+        int thirdNumber = 12;
+        int fourthNumber = 3;
+
+        //when
+        int actualSubtractResult = calculator.subtractTwoNumbers(thirdNumber, fourthNumber);
+
+        //then
+        assertEquals(9, actualSubtractResult); //JUNIT assertEquals
+        assertThat(actualSubtractResult).isEqualTo(9); //Tas pats su ASSERTJ biblioteka
+    }
+
+    @Test
+    void subtractMultipleNumbers() {
+        //given
+        int[] numbertsToTest = {5, 2, 6, 1};
+
+        //when
+        int actualResults = calculator.subtractingMultipleNumbers(numbertsToTest);
+
+        //then
+        assertEquals(-4, actualResults); // pvz dviem variantais
+        assertThat(actualResults).isEqualTo(-4); // pvz dviem variantais, tai naudojam ta kuris labiau patinka
+    }
+
+    @Test
+    void multiplyingTwoNumbers() {
+        //given
+        int firstNumber = 8, secondNumber = 8;
+        //when
+        int actualResult = calculator.multiply(firstNumber, secondNumber);
+        //then
+        assertThat(actualResult).isEqualTo(64);
+
+    }
+
+    @Test
+    void divideTwoNumbers() {
+        int thirdNumber = 4, fourthNumber = 2;
+
+        double actualResult = calculator.divide(thirdNumber, fourthNumber);
+
+        assertThat(actualResult).isEqualTo(2);
+
+    }
+
+    @Test
+    void testDivideTwoNumbersWhereResultIsNotInteger() {
+        int thirdNumber = 9, fourthNumber = 2;
+
+        double actualResult = calculator.divide(thirdNumber, fourthNumber);
+
+        assertThat(actualResult).isEqualTo(4.5);
+    }
+
+//    @Test
+//    void testDivideNumberByZero() {
+//        //given
+//        ArithmeticException exception = assertThrows(
+//                ArithmeticException.class,
+//                () -> calculator.divide(5, 0)
+//        );
+//
+//        assertTrue(exception.getMessage().contains("by zero"));
+    }
+
